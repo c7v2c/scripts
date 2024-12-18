d1529a45f75be1a3e5ddbd3ca7cf5bc440455def
c7v2c <eowjd112500@gmail.com>
2024-12-18 19:42:55 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,24 @@
+local HttpService = game:GetService("HttpService")
 
+local validKeys = {
+    "key1",
+    "key2", 
+    "key3"
+}
+
+local function checkKey(key)
+    for _, validKey in ipairs(validKeys) do
+        if key == validKey then
+            return true
+        end
+    end
+    return false
+end
+
+script_key = ""  
+
+if checkKey(script_key) then
+    print("Key Authorized")
+else
+    print("Invalid Key")
+end
