8526a13ad3005e171efe5e42fdf61bb57982e15e
c7v2c <eowjd112500@gmail.com>
2024-12-18 19:49:56 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,21 +1,20 @@
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
-local validKeys = {
-  "key1",
-  "key2", 
-  "key3"
-}
+
+local validKeys = loadstring(game:HttpGet("https://raw.githubusercontent.com/c7v2c/scripts/main/main.lua"))()
+
 local function checkKey(key)
-  for _, validKey in ipairs(validKeys) do
-      if key == validKey then
-          return true
-      end
-  end
-  return false
+    for _, validKey in ipairs(validKeys) do
+        if key == validKey then
+            return true
+        end
+    end
+    return false
 end
+
 script_key = ""  
 if checkKey(script_key) then
-  print("Key Authorized")
+    print("Key Authorized")
 else
-  player:Kick("구매는 디스코드 c7v2c")
+    player:Kick("키가 올바르지 않아요. 디스코드로 문의해주세요.")
 end
