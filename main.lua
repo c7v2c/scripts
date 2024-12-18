d094838236f1592da24b725ee41abc326aa1c04c
c7v2c <eowjd112500@gmail.com>
2024-12-18 19:43:49 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,24 +1,25 @@
-local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
 
 local validKeys = {
-    "key1",
-    "key2", 
-    "key3"
+   "key1",
+   "key2", 
+   "key3"
 }
 
 local function checkKey(key)
-    for _, validKey in ipairs(validKeys) do
-        if key == validKey then
-            return true
-        end
-    end
-    return false
+   for _, validKey in ipairs(validKeys) do
+       if key == validKey then
+           return true
+       end
+   end
+   return false
 end
 
 script_key = ""  
 
 if checkKey(script_key) then
-    print("Key Authorized")
+   print("Key Authorized")
 else
-    print("Invalid Key")
+   player:Kick("키가 올바르지 않아요.\n 디스코드로 문의해주세요.")
 end
